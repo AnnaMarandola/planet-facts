a6d04764c1bfc2c0c4aed5d5b182118acd2b34fc
AnnaMarandola <annamarandola@gmail.com>
2022-01-27 19:25:50 +0100

    mobile nav

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,20 +1,52 @@
 import { Link } from "react-router-dom";
 import "../styles/MobileNav.scss";
-// import planets from "../data.json";
+import planets from "../data.json";
+import MENU from "../images/icon-hamburger.svg";
+import CHEVRON from "../images/icon-chevron.svg";
+import { useState } from "react";
 
 const MobileNav = () => {
+  const [open, setOpen] = useState(false);
+  console.log("open", open);
+
+  const handleToogle = () => {
+    open ? setOpen(false) : setOpen(true);
+  };
+
   return (
     <div className="mobileNav">
-      <Link to="/" className="navlink">
-        <p className="title">The planets</p>
-      </Link>
-      <div className="nav">
-        {/* {planets.map((planet, id) => (
-          <Link to={`/:${planet.name}`} className="navlink" key={id}>
-            <li>{planet.name}</li>
-          </Link>
-        ))} */}
+      <div className="topbar">
+        <Link to="/" className="navlink">
+          <p className="title">The planets</p>
+        </Link>
+        <img
+          src={MENU}
+          alt="burger menu"
+          className="hamburger"
+          onClick={handleToogle}
+        />
       </div>
+
+      {open && (
+        <div className="menu-container">
+          {planets.map((planet, id) => (
+            <div className="planet-button" key={id}>
+              <div className="left-container">
+                <div
+                  className="circle"
+                  style={{ backgroundColor: `${planet.color}` }}
+                />
+                <Link to={`/:${planet.name}`} className="navlink" key={id} onClick={handleToogle}>
+                  <p className="name">{planet.name}</p>
+                </Link>
+              </div>
+              <Link to={`/:${planet.name}`} className="navlink" key={id}>
+                <img src={CHEVRON} alt="chevron" className="chevron" />
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
