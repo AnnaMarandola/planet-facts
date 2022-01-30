4fb33f302c4e0e0f555a480d1c298d363d13d428
AnnaMarandola <annamarandola@gmail.com>
2022-01-30 16:52:52 +0100

    homepage card animation desktop

--- src/components/PlanetsfactsCard.js
+++ src/components/PlanetsfactsCard.js
@@ -3,7 +3,15 @@ import "../styles/PlanetsfactsCard.scss";
 const PlanetsfactsCard = () => {
   return (
     <div className="link-card">
-      <h1 className="title">planets facts</h1>
+      <div className="border">
+        <h1 className="cardTitle">planets facts</h1>
+      <div className="content">
+        <p className="cardText">
+          Discover the planets of the solar system and their main properties
+        </p>
+        <button className="cardButton">Explore</button>
+      </div>
+      </div>
     </div>
   );
 };
